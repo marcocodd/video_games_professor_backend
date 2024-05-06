ac83b8e18d2461556d7ea7359042a84fdbfbc35e
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-05-06 18:24:13 +0200

    created repository and spring initial setup

---
+++ src/main/java/marco/video_game_professor_backend/VideoGameProfessorBackendApplication.java
@@ -0,0 +1,13 @@
+package marco.video_game_professor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameProfessorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameProfessorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marco/video_game_professor_backend/VideoGameProfessorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package marco.video_game_professor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameProfessorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
