a9e87f00a66da164ca61121889538fc854635bf1
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-05-08 13:01:55 +0200

    Added uploadAvatar with cloudinary

--- src/main/java/marco/video_game_professor_backend/controllers/UserController.java
+++ src/main/java/marco/video_game_professor_backend/controllers/UserController.java
@@ -1,4 +1,28 @@
 package marco.video_game_professor_backend.controllers;
 
+import marco.video_game_professor_backend.entities.User;
+import marco.video_game_professor_backend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PutMapping("/me")
+    public User updateProfile(@AuthenticationPrincipal User currentAuthenticatedUser, @RequestBody User updatedUser) {
+        return this.userService.findByIdAndUpdate(currentAuthenticatedUser.getId(), updatedUser);
+    }
+
+    @PostMapping("/{userId}/avatar")
+    public User uploadAvatar(@RequestParam("avatar") MultipartFile avatar, @PathVariable long userId) throws IOException {
+        return this.userService.uploadAvatar(userId, avatar);
+
+    }
 }
--- src/main/java/marco/video_game_professor_backend/services/UserService.java
+++ src/main/java/marco/video_game_professor_backend/services/UserService.java
@@ -1,5 +1,7 @@
 package marco.video_game_professor_backend.services;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import marco.video_game_professor_backend.entities.User;
 import marco.video_game_professor_backend.exceptions.BadRequestException;
 import marco.video_game_professor_backend.exceptions.NotFoundException;
@@ -8,11 +10,17 @@ import marco.video_game_professor_backend.repositories.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class UserService {
     @Autowired
     private UserDAO userDAO;
+    @Autowired
+    private Cloudinary cloudinary;
+
     @Autowired
     PasswordEncoder passwordEncoder;
 
@@ -40,6 +48,22 @@ public class UserService {
                 "https://ui-avatars.com/api/?name=" + body.userName());
 
         return userDAO.save(newUser);
+    }
+
+    public User findByIdAndUpdate(long userId, User modifiedUser) {
+        User found = this.findById(userId);
+        found.setUserName(modifiedUser.getUserName());
+        found.setEmail(modifiedUser.getEmail());
+        found.setPassword(modifiedUser.getPassword());
+        found.setAvatar(modifiedUser.getAvatar());
+        return this.userDAO.save(found);
+    }
+
+    public User uploadAvatar(long userId, MultipartFile image) throws IOException {
+        User found = this.findById(userId);
+        String url = (String) cloudinary.uploader().upload(image.getBytes(), ObjectUtils.emptyMap()).get("url");
+        found.setAvatar(url);
+        return userDAO.save(found);
     }
 
 }
